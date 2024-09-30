893553aa327910b9c541485716fcbabf47054963
Jacobo Palacio <jacobo.palacio@udea.edu.co>
2024-09-30 00:29:25 -0500

    Adding atomic structure, primary color and font family

---
+++ src/components/atoms/buttons/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => {
+  return <button className='bg-primary w-40 h-14 text-black font-bold py-2 px-4 rounded inline-flex items-center'>
+    <span>Conoce más de mí</span>
+  </button> 
+};
+
+export default Index
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" 
+          rel="stylesheet">
+        </link>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>

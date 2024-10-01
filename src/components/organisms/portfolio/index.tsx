53002fd0428d23f1a4037d809b0d00e46d520973
Jacobo Palacio <jacobo.palacio@udea.edu.co>
2024-10-01 18:51:09 -0500

    Fixing images names errors

--- src/components/organisms/portfolio/index.tsx
+++ src/components/organisms/portfolio/index.tsx
@@ -12,13 +12,13 @@ const projects: Project[] = [
   {
     title: 'FashionApp',
     description: 'A mobile app developed in Kotlin for fashion lovers. It provides fashion tips, outfit ideas, and allows users to buy trendy clothes.',
-    imageUrl: '/fashionapp.png', // Imagen correspondiente al proyecto
+    imageUrl: '/fashion-app.png', // Imagen correspondiente al proyecto
     githubLink: 'https://github.com/jeyko55/fashion-app-movil-kotlin',
   },
   {
     title: 'AppTunelOriente',
     description: 'An app that provides real-time information about the Oriente Tunnel, including traffic, weather, and toll costs.',
-    imageUrl: '/apptuneloriente.png', // Imagen correspondiente al proyecto
+    imageUrl: '/app-tunel-oriente.png', // Imagen correspondiente al proyecto
     githubLink: 'https://github.com/jeyko55/app-tunel-oriente',
   },
   // Puedes añadir más proyectos aquí
